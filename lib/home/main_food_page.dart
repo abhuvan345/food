9038bb5990c30429fc55d92a09bc57117198a313
abhuvan345 <bhuvana.cs24@bmsce.ac.in>
2025-02-26 23:58:17 +0530

    created header of the home page suucessfully !!

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food/widgets/big_text.dart';
+import 'package:food/widgets/small_text.dart';
+
+import '../utils/colors.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({super.key});
@@ -11,6 +15,7 @@ class MainFoodPage extends StatefulWidget {
 class _MainFoodPageState extends State<MainFoodPage> {
   @override
 
+
   Widget build(BuildContext context) {
     return  Scaffold(
       body: Column(
@@ -18,21 +23,30 @@ class _MainFoodPageState extends State<MainFoodPage> {
 
           child: Container(
             margin: EdgeInsets.only(top: 45,bottom: 15),
+            padding: EdgeInsets.only(left: 20,right: 20),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Column(
                   children: [
-                    Text("Country"),
-                    Text("City"),
+                    BigText(text: "India", color: AppColors.mainColor),
+                    Row(
+                      children: [
+                        SmallText(text: "Bangalore", color: Colors.black54,),
+                        Icon(Icons.arrow_drop_down_rounded)
+                      ],
+                    ),
                   ],
                 ),
-                Container(
-                  width: 45,
-                  height: 45,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: Colors.blue,
+                Center(
+                  child: Container(
+                    width: 45,
+                    height: 45,
+                    child: Icon(Icons.search, color: Colors.white,),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: AppColors.mainColor,
+                    ),
                   ),
                 )
               ],
